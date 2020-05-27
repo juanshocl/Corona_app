3297ba0b8e97811b9e93172a2270dbd8250357ca
Toqui012 <alvaroseba2000@gmail.com>
2020-05-26 21:27:16 -0400

    Eliminación Cache

--- ambiente_corona/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\álvaro\desktop\proyectos\corona_app\ambiente_corona\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
